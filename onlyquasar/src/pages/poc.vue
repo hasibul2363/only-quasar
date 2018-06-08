46b149876fd7fd95c5e47939d959b89c2600c15f
hasibul2363 <hasibul2363@gmail.com>
2018-06-08 22:50:37 +0600

    applied component loading

--- onlyquasar/src/pages/poc.vue
+++ onlyquasar/src/pages/poc.vue
@@ -1,28 +1,38 @@
 <template>
   <q-page class="row justify-center"  style="background-color: black; color: white">
 <div>
-    <text-box-biz :schema="formSchema.txt1"></text-box-biz>
-    <text-box-number-biz :schema="formSchema.txt2"></text-box-number-biz>
-    <text-box-formula-biz :schema="formSchema.txtSalary" :formschema="formSchema"></text-box-formula-biz>
+
+    <div v-for="schema in formSchema" :key="schema.name">
+      <component :is="bizComponents[schema.controlType]" :schema="schema" :formschema="formSchema"></component>
+    </div>
+
+    <!-- <text-box-biz :schema="formSchema.txt1"></text-box-biz> -->
+    <!-- <text-box-number-biz :schema="formSchema.txt2"></text-box-number-biz> -->
+    <!-- <text-box-formula-biz :schema="formSchema.txtSalary" :formschema="formSchema"></text-box-formula-biz> -->
+    <!-- <component :is="textBoxBiz" :schema="formSchema.txt1"></component> -->
+
+
     <input type="button" @click="test" value="test"/>
 </div>
-
   </q-page>
  </template>
 <script>
-
-
 import textBoxBiz from "../controls/formBuilder/text-box-biz";
 import textBoxNumberBiz from "../controls/formBuilder/text-box-number-biz";
 import textBoxFormulaBiz from "../controls/formBuilder/text-box-formula-biz";
 export default {
-  components: {
+  /*components: {
     textBoxBiz: textBoxBiz,
     textBoxNumberBiz: textBoxNumberBiz,
     textBoxFormulaBiz: textBoxFormulaBiz
-  },
+  },*/
   data() {
     return {
+      bizComponents:{
+        textBoxBiz: textBoxBiz,
+         textBoxNumberBiz: textBoxNumberBiz,
+         textBoxFormulaBiz: textBoxFormulaBiz
+      },
       text: "Hello world",
       formSchema: {
         txt1: {
@@ -33,7 +43,8 @@ export default {
           readonly: false,
           maxLength: "5",
           minLength: "2",
-          required: true
+          required: true,
+          controlType:"textBoxBiz"
         },
         txt2: {
           label: "Your Age",
@@ -43,7 +54,8 @@ export default {
           readonly: false,
           maxValue: 5,
           minValue: 2,
-          required: false
+          required: false,
+          controlType:"textBoxNumberBiz"
         },
         txtSalary: {
           label: "Salary",
@@ -52,7 +64,8 @@ export default {
           formulaTemplate: "{txt2} * 2",
           placeholder: "here is your salary",
           readonly: false,
-          required: false
+          required: false,
+          controlType:"textBoxFormulaBiz"
         }
       }
     };
@@ -61,6 +74,11 @@ export default {
     test() {
       alert(JSON.stringify(this.formSchema));
     }
+  },
+  created(){
+    /*this.bizComponents["textBoxBiz"] = () => import("../controls/formBuilder/text-box-biz");
+    this.bizComponents["textBoxNumberBiz"] = () => import("../controls/formBuilder/text-box-number-biz");
+    this.bizComponents["textBoxFormulaBiz"] = () => import("../controls/formBuilder/text-box-formula-biz");    */
   }
 };
 </script>
